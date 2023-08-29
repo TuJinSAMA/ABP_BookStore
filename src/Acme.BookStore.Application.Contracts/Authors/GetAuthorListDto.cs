6636eddc06811f495c50f4b82dc11fd7029d8760
gongyuqi <gong_yu_qi@163.com>
2023-08-29 18:01:00 +0800

    initialize project

---
+++ src/Acme.BookStore.Application.Contracts/Authors/GetAuthorListDto.cs
@@ -0,0 +1,9 @@
+﻿using JetBrains.Annotations;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Authors;
+
+public class GetAuthorListDto : PagedAndSortedResultRequestDto
+{
+    public string? Filter { get; set; }
+}
